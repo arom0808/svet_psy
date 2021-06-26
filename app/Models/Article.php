f228d8444e6d8ffdbb54e791b32fea7332c82597
Roman <arom0808@mail.ru>
2021-06-26 13:34:20 +0300

    first commit

---
+++ app/Models/Article.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'id';
+    protected $guarded = [];
+
+    public function quotes()
+    {
+        return $this->hasMany('App\Models\Quote', 'category_id', 'id');
+    }
+}
---
+++ database/migrations/2021_06_26_084725_create_quotes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateQuotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quotes', function (Blueprint $table) {
+            $table->id();
+            $table->text('text');
+            $table->string('author');
+            $table->bigInteger('category_id',false,true)->nullable();
+            $table->boolean('isQuoteOfDay')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quotes');
+    }
+}

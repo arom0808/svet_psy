66e23d3db92c8efe78cbdb9758b2a23202ee7cb9
Roman <arom0808@mail.ru>
2021-07-06 21:21:37 +0300

    some bugfixes

--- resources/views/livewire/quotes-list.blade.php
+++ resources/views/livewire/quotes-list.blade.php
@@ -4,7 +4,7 @@
         <div class="text-end">
             {{-- <form class="flex w-full max-w-sm space-x-3"> --}}
                 <div class=" relative ">
-                    <input wire:input="search($event.target.value)" type="text" id="&quot;form-subscribe-Search"
+                    <input wire:input="search($event.target.value)" onchange="this.blur()" type="text" id="&quot;form-subscribe-Search"
                         class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
                         placeholder="Enter" />
                 </div>

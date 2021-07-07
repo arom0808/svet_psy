2e18f57e025e36b52515ab083205a5b9c98a9faf
Roman <arom0808@mail.ru>
2021-07-07 21:40:36 +0300

    Enter -> Search

--- resources/views/livewire/quotes-list.blade.php
+++ resources/views/livewire/quotes-list.blade.php
@@ -6,7 +6,7 @@
                 <div class=" relative ">
                     <input wire:change="search($event.target.value)" onchange="this.blur(); history.pushState(null, null, '/quotes/'+this.value);" type="text" id="&quot;form-subscribe-Search"
                         class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-                        value="{{$searchExpression}}" placeholder="Enter" />
+                        value="{{$searchExpression}}" placeholder="Search" />
                 </div>
                 {{-- <button
                     class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
